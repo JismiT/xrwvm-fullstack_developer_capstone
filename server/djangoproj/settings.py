3dd33e14124c862e827378494309e86a8b22c9f3
JismiT <jismitinujacob@gmail.com>
2025-11-14 12:49:04 +0200

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -27,11 +27,10 @@ SECRET_KEY =\
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = ['localhost', + \
-        'https://jismitinujac-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = + \
-['https://jismitinujac-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+HOST = 'https://jismitinujac-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+ORGN = 'https://jismitinujac-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+ALLOWED_HOSTS = ['localhost', HOST]
+CSRF_TRUSTED_ORIGINS = [ORGN]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -96,9 +95,9 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.' + \
-        'UserAttributeSimilarityValidator',
-    }, 
+        'django.contrib.auth.password_validation.
+        UserAttributeSimilarityValidator',
+    },
     {
         'NAME':
         'django.contrib.auth.password_validation.MinimumLengthValidator',
